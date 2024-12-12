87c5c863782548efa43ce43549c3e8ec165ac994
mahmoudAbdelraoufHassan <mahmoud.abdelraouf.khalil@gmail.com>
2024-12-12 07:12:39 +0200

    convert image type

--- src/app/containers/blog/blog.component.ts
+++ src/app/containers/blog/blog.component.ts
@@ -17,22 +17,22 @@ articles : article[];
 constructor(){
   this.articles = [
     {title : "GPT-3 and Open  AI is the future. Let us exlore how it is?",
-      img : "assets/blog02.png",
+      img : "assets/blog02.webp",
       date : "Sep 26, 2021"
     },
 
     {title : "GPT-3 and Open  AI is the future. Let us exlore how it is?",
-      img : "assets/blog03.png",
+      img : "assets/blog03.webp",
       date : "Sep 26, 2021"
     },
 
     {title : "GPT-3 and Open  AI is the future. Let us exlore how it is?",
-      img : "assets/blog04.png",
+      img : "assets/blog04.webp",
       date : "Sep 26, 2021"
     },
 
     {title : "GPT-3 and Open  AI is the future. Let us exlore how it is?",
-      img : "assets/blog05.png",
+      img : "assets/blog05.webp",
       date : "Sep 26, 2021"
     },
 
